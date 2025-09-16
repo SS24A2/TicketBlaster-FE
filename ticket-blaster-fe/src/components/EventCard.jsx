ea7a7f0161bf8a459eb3923649fb8e258d607d44
SS24A2 <ana.janevska21@yahoo.com>
2025-09-16 22:37:35 +0200

    add style, modify event name in event card

--- ticket-blaster-fe/src/components/EventCard.jsx
+++ ticket-blaster-fe/src/components/EventCard.jsx
@@ -1,13 +1,18 @@
 import convertDate from '../convertDate'
 
-export default function EventCard({ event, imageSrc, ButtonComponent }) {
+export default function EventCard({
+    event,
+    imageSrc,
+    ButtonComponent,
+    UpdateLink,
+}) {
     return (
         <div className="event-card">
             <div className="event-image">
                 <img src={imageSrc} alt="event" />
             </div>
             <div className="event-info">
-                <h4>{event.name}</h4>
+                {!UpdateLink ? <h4>{event.name}</h4> : UpdateLink}
                 <h5>{convertDate(event.date)}</h5>
                 <p>{event.details.substring(0, 150)}...</p>
                 <div>
