1fd5f8dc47ff51968db7c4ec5e901f6d0bb67c0c
SS24A2 <ana.janevska21@yahoo.com>
2025-10-28 01:10:17 +0100

    adjust for printing

--- ticket-blaster-fe/src/components/TicketsModal.jsx
+++ ticket-blaster-fe/src/components/TicketsModal.jsx
@@ -22,54 +22,66 @@ export default function TicketsModal({
                 }, 500)
             }}
         >
-            <div className="modal-tickets">
-                <div
-                    onClick={(e) => {
-                        e.stopPropagation()
-                        setIsTicketsModalOpen(false)
-                        setTicketIndex(0)
-                    }}
-                    dangerouslySetInnerHTML={{
-                        __html: ticketsforSelectedEvent[ticketIndex].ticket,
-                    }}
-                ></div>
-                {ticketsforSelectedEvent.length > 1 &&
-                    ticketIndex < ticketsforSelectedEvent.length - 1 && (
-                        <span
+            <section className="all-tickets">
+                {ticketsforSelectedEvent.map((t, index) => (
+                    <div
+                        className="modal-tickets"
+                        key={index}
+                        style={{
+                            display: index === ticketIndex ? 'block' : 'none',
+                        }}
+                    >
+                        <div
                             onClick={(e) => {
                                 e.stopPropagation()
-                                setTicketIndex(
-                                    (ticketIndex + 1) %
-                                        ticketsforSelectedEvent.length
-                                )
+                                setIsTicketsModalOpen(false)
+                                setTicketIndex(0)
                             }}
-                            className="next-ticket"
-                        >
-                            <img src={nextIcon} alt="next" />
-                        </span>
-                    )}
-                {ticketsforSelectedEvent.length > 1 && ticketIndex > 0 && (
-                    <span
-                        onClick={(e) => {
-                            e.stopPropagation()
-                            setTicketIndex(
-                                (ticketIndex +
-                                    ticketsforSelectedEvent.length -
-                                    1) %
-                                    ticketsforSelectedEvent.length
-                            )
-                        }}
-                        className="previous-ticket"
-                    >
-                        <img src={previousIcon} alt="previous" />
-                    </span>
-                )}
-                {ticketsforSelectedEvent.length > 1 && (
-                    <h5 className="ticket-num">
-                        Ticket number: {ticketIndex + 1}
-                    </h5>
-                )}
-            </div>
+                            dangerouslySetInnerHTML={{
+                                __html: ticketsforSelectedEvent[index].ticket,
+                            }}
+                        ></div>
+                        {ticketsforSelectedEvent.length > 1 &&
+                            ticketIndex <
+                                ticketsforSelectedEvent.length - 1 && (
+                                <span
+                                    onClick={(e) => {
+                                        e.stopPropagation()
+                                        setTicketIndex(
+                                            (ticketIndex + 1) %
+                                                ticketsforSelectedEvent.length
+                                        )
+                                    }}
+                                    className="next-ticket"
+                                >
+                                    <img src={nextIcon} alt="next" />
+                                </span>
+                            )}
+                        {ticketsforSelectedEvent.length > 1 &&
+                            ticketIndex > 0 && (
+                                <span
+                                    onClick={(e) => {
+                                        e.stopPropagation()
+                                        setTicketIndex(
+                                            (ticketIndex +
+                                                ticketsforSelectedEvent.length -
+                                                1) %
+                                                ticketsforSelectedEvent.length
+                                        )
+                                    }}
+                                    className="previous-ticket"
+                                >
+                                    <img src={previousIcon} alt="previous" />
+                                </span>
+                            )}
+                        {ticketsforSelectedEvent.length > 1 && (
+                            <h5 className="ticket-num">
+                                Ticket number:{index + 1}
+                            </h5>
+                        )}
+                    </div>
+                ))}
+            </section>
         </div>
     )
 }
